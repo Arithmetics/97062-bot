2fdaad946f1be548ee50daa6bbec450a7e64e2a3
Arithmetics <brock.m.tillotson@gmail.com>
2021-04-12 21:40:57 -0700

    save

--- cypress/integration/scoreboard/scrape.spec.js
+++ cypress/integration/scoreboard/scrape.spec.js
@@ -5,7 +5,7 @@ context("scraping", () => {
     window.$ = Cypress.$;
     console.log(window.$);
 
-    cy.visit("https://sports.oregonlottery.org/sports/");
+    cy.visit("https://sports.oregonlottery.org/sports/basketball/nba/");
 
     cy.get(".rj-carousel-container")
       .find(".rj-carousel-item__sportName")
@@ -19,22 +19,26 @@ context("scraping", () => {
       .click();
     cy.get(".events-container-upcoming")
       .find(".rj-ev-list__ev-card")
-      .each(($el) => {
-        const team1 = $el.find(".rj-ev-list__ev-card__team-1-name").text();
-        const team2 = $el.find(".rj-ev-list__ev-card__team-2-name").text();
+      .eq(0)
+      .find(".rj-ev-list__ev-card__team-1-name")
+      .should("contain", "TOR");
+    console.log(x);
+    // .each(($el) => {
+    //   const team1 = $el.find(".rj-ev-list__ev-card__team-1-name").text();
+    //   const team2 = $el.find(".rj-ev-list__ev-card__team-2-name").text();
 
-        $el
-          .find(".rj-ev-list__ev-card__section--TwoBox")
-          .find(".rj-ev-list__bet-btn__point")
-          .each(($el2, i) => {
-            if (i === 0) {
-              const line1 = $el2.text();
-              console.log(team1, line1);
-            }
-            if (i === 0) {
-              const line2 = $el2.text(team2, line2);
-            }
-          });
-      });
+    //   $el
+    //     .find(".rj-ev-list__ev-card__section--TwoBox")
+    //     .find(".rj-ev-list__bet-btn__point")
+    //     .each(($el2, i) => {
+    //       if (i === 0) {
+    //         const line1 = $el2.text();
+    //         console.log(team1, line1);
+    //       }
+    //       if (i === 0) {
+    //         const line2 = $el2.text(team2, line2);
+    //       }
+    //     });
+    // });
   });
 });
---
+++ scrape.js
@@ -0,0 +1,63 @@
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+const url = "https://sports.oregonlottery.org/sports/basketball/nba";
+
+function parseGames(html) {
+  const availableGames = [];
+  const $ = cheerio.load(html);
+  const contentBlocks = $("#league-view").find(".rj-ev-list__content");
+  for (let i = 0; i < contentBlocks.length; i++) {
+    try {
+      const block = contentBlocks[i];
+
+      const date = $(block).find(".rj-ev-list__heading-title").text();
+
+      const games = $(block).find(".rj-ev-list__ev-card");
+
+      for (let i = 0; i < games.length; i++) {
+        const game = games[i];
+        try {
+          const awayTeam = $(game)
+            .find(".rj-ev-list__ev-card__team-1-name")
+            .text();
+          const homeTeam = $(game)
+            .find(".rj-ev-list__ev-card__team-2-name")
+            .text();
+
+          const betBlocks = $(game).find(".rj-ev-list__bet-btn__row");
+
+          const awayLine = $(betBlocks[0]).find("span").text();
+
+          const over = $(betBlocks[10]).find("span").text();
+
+          availableGames.push({
+            headerText,
+            awayTeam,
+            homeTeam,
+            awayLine,
+            over,
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  return availableGames;
+}
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForSelector(".league-events-block");
+  await new Promise((r) => setTimeout(r, 2000));
+  const content = await page.content();
+  parseGames(content);
+
+  await browser.close();
+})();
